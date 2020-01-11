ba134b7db46fff652eea99eff5cc54a740f80fb2
Thomas Daede <daede003@umn.edu>
2020-01-10 16:09:09 -0800

    alternate video

--- switch_demo.sh
+++ switch_demo.sh
@@ -2,7 +2,7 @@
 
 cargo build --release
 
-parallel target/release/rav1e --tune Psnr --bitrate {} -s 10 -l 1800 --tiles 16 ~/Videos/DOTA2_480p.y4m --low-latency -o dota_switch_{}.ivf --switch-frame-interval 30 --rdo-lookahead-frames=1 --min-keyint=240 --keyint=240 ::: 400 1000
+parallel target/release/rav1e --tune Psnr --bitrate {} -s 10 -l 1800 --tiles 16 ~/Videos/raw-852x480.y4m --low-latency -o dota_switch_{}.ivf --switch-frame-interval 30 --rdo-lookahead-frames=1 --min-keyint=240 --keyint=240 ::: 400 1000
 
 python3 ~/Downloads/stitch.py --sequence 120:1,360:0,600:1,840:0,1080:1,1320:0,1560:1 dota_switch_1000.ivf dota_switch_400.ivf -o dota_stitched.ivf
 
